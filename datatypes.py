bf2e17f805637603cfb11755c6db3929fccae5bc
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-09-17 21:13:51 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,50 @@
+data = "1234"
+print(data)
+print(type(data))
+print("_______________________")
+
+data = 65044
+print(data)
+print(type(data))
+
+print("_______________________")
+
+data = 32.98
+print(data)
+print(type(data))
+print("_______________________")
+
+data = 6+3j
+print(data)
+print(type(data))
+print("_______________________")
+
+data = range(2,60)
+print(data)
+print(type(data))
+print("_______________________")
+
+data = [88,46,32,12,19,82]
+print(data)
+print(type(data))
+
+print("_______________________")
+
+data = (87,21,45,56,69)
+print(data)
+print(type(data))
+print("_______________________")
+
+data = {6,5,0,4,4.4}
+print(data)
+print(type(data))
+print("_______________________")
+
+data = {'name':'Waseem','city':'Solapur', 'gender':'Male'}
+print(data)
+print(type(data))
+print("_______________________")
+
+data = False
+print(data)
+print(type(data))
---
+++ user-input.py
@@ -0,0 +1,7 @@
+name = input('ENTER YOUR NAME - ')
+print(f'My name is {name}')
+
+x = int(input('Enter a number - '))
+y = int(input('ENTER A NUMBER - '))
+z = x + y
+print(z)
