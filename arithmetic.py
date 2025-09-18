4196a06cc377bff3340da94434bf92ab9e5933f4
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-09-18 21:22:29 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,19 @@
+# Arithmetic	
+#     Addition
+# 	Subtraction
+# 	Multiplication
+# 	Division
+# 	Floor Division
+# 	Modulus
+# 	Exponential
+x = 33
+y = 5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+x = 2
+y = 4
+print(x**y)
---
+++ assignment.py
@@ -0,0 +1,17 @@
+x = 20
+print(x)
+
+y = x
+print(y)
+
+z = x + y
+print(z)
+
+x += 10 #x = x + 10
+print(x)
+
+x-=5
+print(x)
+
+x *= 10
+print(x)
